4a03322ea6ea5621b302568c7abf2b5656522bd7
Bilal Hussain <bilalhussain786b@gmail.com>
2019-04-12 18:43:11 +0500

    errors removed

--- app/src/main/java/com/home/bilalhussain/myhomecontroller/RegisterActivity.java
+++ app/src/main/java/com/home/bilalhussain/myhomecontroller/RegisterActivity.java
@@ -17,12 +17,14 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.firebase.client.Firebase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity {
-    EditText username, password;
+    EditText id, username, password, ph;
     Button registerButton;
     String user, pass;
     TextView login;
@@ -60,65 +62,73 @@ public class RegisterActivity extends AppCompatActivity {
                 else if(!user.matches("[A-Za-z0-9]+")){
                     username.setError("only alphabet or number allowed");
                 }
-                else if(user.length()<5){
+                else if(user.length()<1){
                     username.setError("at least 5 characters long");
                 }
-                else if(pass.length()<5){
+                else if(pass.length()<8){
                     password.setError("at least 5 characters long");
                 }
                 else {
-                    register();
+                   // register();
                         // function here
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference myRef = database.getReference("ID");
+
+
+                    myRef.child("Name").setValue("Hello, World!");
+                    myRef.child("Password").setValue("Hello, World!");
+                    myRef.child("Phone").setValue("Hello, World!");
+
                 }
             }
         });
     }
-    private void register(){
-        final ProgressDialog pd = new ProgressDialog(RegisterActivity.this);
-        pd.setMessage("Loading...");
-        pd.show();
-
-        String url = "https://smarthomecontroller-1371a.firebaseio.com/ID.json";
-
-        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>(){
-            @Override
-            public void onResponse(String s) {
-                Firebase reference = new Firebase("https://smarthomecontroller-1371a.firebaseio.com/ID");
-
-                if(s.equals("null")) {
-                    reference.child(user).child("password").setValue(pass);
-                    Toast.makeText(RegisterActivity.this, "registration successful", Toast.LENGTH_LONG).show();
-                }
-                else {
-                    try {
-                        JSONObject obj = new JSONObject(s);
-
-                        if (!obj.has(user)) {
-
-                            reference.child(user).child("password").setValue(pass);
-                            Toast.makeText(RegisterActivity.this, "registration successful", Toast.LENGTH_LONG).show();
-                        } else {
-                            reference.child(user).child("password").setValue(pass);
-                            Toast.makeText(RegisterActivity.this, "username already exists", Toast.LENGTH_LONG).show();
-                        }
-
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                pd.dismiss();
-            }
-
-        },new Response.ErrorListener(){
-            @Override
-            public void onErrorResponse(VolleyError volleyError) {
-                System.out.println("" + volleyError );
-                pd.dismiss();
-            }
-        });
-
-        RequestQueue rQueue = Volley.newRequestQueue(RegisterActivity.this);
-        rQueue.add(request);
-    }
+//    private void register(){
+//        final ProgressDialog pd = new ProgressDialog(RegisterActivity.this);
+//        pd.setMessage("Loading...");
+//        pd.show();
+//
+//        String url = "https://smarthomecontroller-1371a.firebaseio.com/ID.json";
+//
+//        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>(){
+//            @Override
+//            public void onResponse(String s) {
+//                Firebase reference = new Firebase("https://smarthomecontroller-1371a.firebaseio.com/ID");
+//
+//                if(s.equals("null")) {
+//                    reference.child(user).child("password").setValue(pass);
+//                    Toast.makeText(RegisterActivity.this, "registration successful", Toast.LENGTH_LONG).show();
+//                }
+//                else {
+//                    try {
+//                        JSONObject obj = new JSONObject(s);
+//
+//                        if (!obj.has(user)) {
+//
+//                            reference.child(user).child("password").setValue(pass);
+//                            Toast.makeText(RegisterActivity.this, "registration successful", Toast.LENGTH_LONG).show();
+//                        } else {
+//                            reference.child(user).child("password").setValue(pass);
+//                            Toast.makeText(RegisterActivity.this, "username already exists", Toast.LENGTH_LONG).show();
+//                        }
+//
+//                    } catch (JSONException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//
+//                pd.dismiss();
+//            }
+//
+//        },new Response.ErrorListener(){
+//            @Override
+//            public void onErrorResponse(VolleyError volleyError) {
+//                System.out.println("" + volleyError );
+//                pd.dismiss();
+//            }
+//        });
+//
+//        RequestQueue rQueue = Volley.newRequestQueue(RegisterActivity.this);
+//        rQueue.add(request);
+//    }
 }
